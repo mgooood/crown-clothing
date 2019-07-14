aca8f1692214d53e6a05fde8eeb1648d2137d42d
Mark Good <mark.gooood@gmail.com>
2019-07-14 19:44:20 -0400

    adding basic routing for hats page

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom'; 
 import './menu-item.styles.scss';
 
-const menuItem = ({ title, imageUrl, size }) => (
-  <div className={`${size ? size + " ": ""}menu-item`}>
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+  <div 
+    className={`${size ? size + " ": ""}menu-item`} 
+    onClick={() => history.push(`${match.url}${linkUrl}`)}>
     <div className="background-image" style= {{backgroundImage: `url(${imageUrl})`}} />
     <div className="content">
       <h1 className="title">{title.toUpperCase()}</h1>
@@ -11,4 +14,4 @@ const menuItem = ({ title, imageUrl, size }) => (
   </div>
 );
 
-export default menuItem;
+export default withRouter(MenuItem);
