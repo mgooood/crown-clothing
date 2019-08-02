a825ff868719414aa6b0acbbee8aa3c9df7278b6
Mark Good <mark.gooood@gmail.com>
2019-08-02 13:21:42 -0400

    integrated google sign in with firebase

--- src/App.js
+++ src/App.js
@@ -6,17 +6,43 @@ import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
-        <Route path='/signin' component={SignInAndSignUp} />
-      </Switch>
-    </div>
-  );
+import { auth } from './firebase/firebase.utils';
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+      console.log(user);
+      console.log(this.unsubsubscribeFromAuth);
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignInAndSignUp} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import FormInput from '../form-input/form-input.component.jsx';
+import CustomButton from '../custom-button/custom-button.component.jsx';
+import { signInWithGoogle } from '../../firebase/firebase.utils';
 import './sign-in.styles.scss';
 
 class SignIn extends React.Component {
@@ -28,9 +30,16 @@ class SignIn extends React.Component {
         <h2>I already have an account</h2>
         <span>Sign in with your email and password</span>
         <form onSubmit={this.handleSubmit}>
-          <FormInput type="email" name="email" handleChange={this.handleChange} value={this.state.email} label="email" required />
-          <FormInput type="password" name="password" handleChange={this.handleChange} value={this.state.password} label="password" required />
-          <input type="submit" value="Submit Form" />
+          <div className="group">
+            <FormInput type="email" name="email" handleChange={this.handleChange} value={this.state.email} label="email" required />
+          </div>
+          <div className="group">
+            <FormInput type="password" name="password" handleChange={this.handleChange} value={this.state.password} label="password" required />
+          </div>
+          <div className="buttons">
+            <CustomButton type="submit">Sign In</CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>Sign in with Google</CustomButton>
+          </div>
         </form>
       </div>
     )
